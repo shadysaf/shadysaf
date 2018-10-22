39ecc896e9febc48f0eb0d238a239229c83ebfc2
user <shadysafadi78@gmail.com>
2018-10-22 13:34:07 +0300

    PlayersList

--- app/src/main/java/com/example/user/shadysaf/PLAYERLISTActivity.java
+++ app/src/main/java/com/example/user/shadysaf/PLAYERLISTActivity.java
@@ -2,12 +2,36 @@ package com.example.user.shadysaf;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class PLAYERLISTActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class PLAYERLISTActivity extends AppCompatActivity implements AdapterView.OnClickListener{
+    ArrayList<Item> items;
+    CustomAdapter arrayAdapter;
+    ListView playersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playerlist);
+
+        playersList = findViewById(R.id.playersList);
+        items = new ArrayList<>();
+        items.add(new Item(R.drawable.stegen,"shady"));
+        items.add(new Item(R.drawable.stegen,"shady"));
+        items.add(new Item(R.drawable.stegen,"shady"));
+        items.add(new Item(R.drawable.stegen,"shady"));
+
+        arrayAdapter = new CustomAdapter(this,R.layout.custom_row,items);
+        playersList.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
