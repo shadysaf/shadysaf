acb9fa1ea896e97d315f8fd5706b8946b991654e
user <shadysafadi78@gmail.com>
2018-11-26 13:48:24 +0200

    GamesList

--- app/src/main/java/com/example/user/shadysaf/CameraGalleryActivity.java
+++ app/src/main/java/com/example/user/shadysaf/CameraGalleryActivity.java
@@ -2,9 +2,11 @@ package com.example.user.shadysaf;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +14,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CameraGalleryActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -52,12 +59,19 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-     //   if (requestCode == CAMERA_REQUEST && requestCode == Activity.RESULT_OK){
+        if (requestCode == CAMERA_REQUEST && requestCode == Activity.RESULT_OK){
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+            String imagepath = saveImage(bitmap);
 
-    //    }
-         if(requestCode == SELECT_IMAGE && requestCode == Activity.RESULT_OK){
+            SharedPreferences pref = getSharedPreferences("mypref",MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString("image",imagepath);
+            editor.commit();
+
+
+        }
+         else if(requestCode == SELECT_IMAGE && requestCode == Activity.RESULT_OK){
             Uri targetUri = data.getData();
             try{
                 bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
@@ -67,6 +81,25 @@ public class CameraGalleryActivity extends AppCompatActivity implements View.OnC
             }
         }
 
+    }
+    public String saveImage (Bitmap bitmap){
+        File root = Environment.getExternalStorageDirectory();
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String filePath = root.getAbsolutePath()+"/DCIM/Camera/IMG_"+timeStamp+".jpg";
+        File file = new File(filePath);
+        try
+        {
+            file.createNewFile();
+            FileOutputStream ostream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100,ostream);
+            ostream.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            Toast.makeText(this,"Faild to save image",Toast.LENGTH_SHORT).show();
+        }
+        return filePath;
     }
 
 }
