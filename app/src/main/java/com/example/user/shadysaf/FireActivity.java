fccbcff536fd70a6dd73d48da40f538a5bbcb358
user <shadysafadi78@gmail.com>
2018-12-12 14:19:45 +0200

    GamesList

--- app/src/main/java/com/example/user/shadysaf/FireActivity.java
+++ app/src/main/java/com/example/user/shadysaf/FireActivity.java
@@ -3,13 +3,21 @@ package com.example.user.shadysaf;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,41 +26,59 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
-public class FireActivity extends AppCompatActivity {
+public class FireActivity extends AppCompatActivity implements View.OnClickListener {
     EditText etEmail,etPass;
-    Button btSave;
-    TextView tvEmail,tvProfession;
-    ListView lvUsers;
-    ArrayList<String>users;
+    Button btSave,button10;
+    private FirebaseAuth mAuth;
+    final String TAG ="Firebase";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fire);
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference myRef = database.getReference().child("name");
+        mAuth = FirebaseAuth.getInstance();
         etEmail = (EditText) findViewById(R.id.etEmail);
         etPass = (EditText) findViewById(R.id.etPass);
         btSave = (Button) findViewById(R.id.btSave);
-        tvEmail = (TextView) findViewById(R.id.tvEmail);
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String value = dataSnapshot.getValue(String.class);
-                tvEmail.setText(value);
-            }
+        button10 = (Button) findViewById(R.id.button10);
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
-        tvProfession = (TextView) findViewById(R.id.tvProfession);
-        lvUsers = (ListView) findViewById(R.id.lvUsers);
-        users = new ArrayList<String>();
-        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,users);
-        lvUsers.setAdapter(adapter);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+    public void createAccount(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(FireActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(FireActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
 
+                        // ...
+                    }
+                });
+    }
+
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
